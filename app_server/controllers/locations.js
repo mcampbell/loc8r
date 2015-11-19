9e6eccd34fbbcf37a8aef5d7db6070b7cd0a0ede
Michael Campbell <michael.campbell@gmail.com>
2015-11-18 19:38:47 -0500

    feat: Added list view
    * Added list view with static text, but responsive sections.  Section
      4.3

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -12,3 +12,7 @@ module.exports.locationInfo = function(req, res){
 module.exports.addReview = function(req, res){
     res.render('index', { title: 'Add review' });
 };
+
+module.exports.homelist = function(req, res) {
+    res.render('locations-list', {title: 'Home'});
+};
